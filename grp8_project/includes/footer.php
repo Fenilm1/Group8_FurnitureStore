609121dfbd42e1e50bdb5ea5c8eb20f134ffa087
NidhiPatel09 <nid.000059@gmail.com>
2024-08-06 15:24:11 -0400

    Design changes

--- grp8_project/includes/footer.php
+++ grp8_project/includes/footer.php
@@ -1,11 +1,32 @@
 <footer class="footer bg-dark text-white mt-5">
-    <div class="container text-center py-3">
-        <span class="text-muted">&copy; 2024 E-commerce Website</span>
+    <div class="item33">
+        <h3>Store hours</h3>
+        <ul>
+            <li>Monday-Saturday: 10 AM to 5 PM</li>
+            <li>Sunday: Closed</li>
+        </ul>
     </div>
+    <div class="item33">
+        <h3>Stores</h3>
+        <ul>
+            <li>Toronto</li>
+            <li>Waterloo</li>
+        </ul>
+    </div>
+    <div class="item33">
+        <h3>Quick links</h3>
+        <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="../login.php">Login</a></li>
+            <li><a href="../register.php">Register</a></li>
+        </ul>
+    </div>
+    <p id="copyright"> &copy; Group-8 Furniture Store</p>
 </footer>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
 <script src="js/scripts.js"></script>
 </body>
+
 </html>
